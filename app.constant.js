1144757b6d5b9d0ab2c2be1d38855207e60e3902
HungBui-s4019314 <s4019314@rmit.edu.vn>
2026-06-09 22:42:16 +0700

    Delete app.constant.js

--- app.constant.js
+++
@@ -1,7 +0,0 @@
-const APP_CONSTANT = {
-  PORT: process.env.PORT || 3000,
-  COOKIE_SECRET: process.env.COOKIE_SECRET || "mySecretKey",
-  COOKIE_MAX_AGE: 24 * 60 * 60 * 1000, // 1 day
-};
-
-export default APP_CONSTANT;
