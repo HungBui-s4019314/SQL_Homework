fc2733edd0dc83d06616daf955d6382f7db0b239
HungBui-s4019314 <s4019314@rmit.edu.vn>
2026-06-10 00:34:09 +0700

    Create response.helper.js

---
+++ common/helpers/response.helper.js
@@ -0,0 +1,15 @@
+const responseSuccess = (res, data, statusCode = 200) => {
+  return res.status(statusCode).json({
+    success: true,
+    data,
+  });
+};
+
+const responseError = (res, message, statusCode = 500) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
+
+export default { responseSuccess, responseError };
