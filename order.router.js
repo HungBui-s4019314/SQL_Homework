d462c5cdf22ce6cb628065850e2a5ffc688a6696
HungBui-s4019314 <s4019314@rmit.edu.vn>
2026-06-09 22:44:58 +0700

    Delete order.router.js

--- order.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import orderController from "../controllers/order.controller.js";
-
-const router = Router();
-
-router.post("/", orderController.createOrder);
-router.get("/user/:userId", orderController.getOrdersByUser);
-
-export default router;
